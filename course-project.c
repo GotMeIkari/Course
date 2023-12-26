df390f055b9384fc33227cd65da72dda4c11dc62
Никита Минаков <144937189+GotMeIkari@users.noreply.github.com>
2023-12-26 18:37:26 +0300

    Update course-project.c

--- course-project.c
+++ course-project.c
@@ -1,26 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string.h> 
+#include <stdbool.h>
 #include <locale.h>
 #include <stdio.h>
 /*подключаемые заголовочные файлы*/
 #define ROW 6
 #define COL 10
-#define ROW_1 (int[COL]){5, 3, 0, 8, 7, 2, 1, 9, 6, 4} 
-#define ROW_2 (int[COL]){6, 8, 4, 9, 3, 5, 7, 2, 1, 0}
-#define ROW_3 (int[COL]){0, 7, 3, 2, 6, 1, 4, 5, 8, 9}
-#define ROW_4 (int[COL]){2, 5, 1, 4, 3, 0, 8, 9, 7, 6}
-#define ROW_5 (int[COL]){7, 0, 3, 5, 8, 4, 6, 2, 1, 9}
 /*константы для настройки работы приложения*/
 
+int load_file(int array[ROW - 1][COL]);
 void draw_screen(int game_tablet[ROW][COL], int, int);
-/*конструктор для структуры draw_screen*/
-
 void answer_peak(int gametablet[ROW][COL]);
 
-void finish_flag();
+void finish_flag_easy();
+void finish_flag_normal();
+void finish_flag_hard();
+
+void choose_path();
+void menu_peak();
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    finish_flag();
+    menu_peak();
 }
 
 
@@ -48,10 +51,13 @@ void draw_screen(int game_tablet[ROW][COL], int height, int width) {
     }
 }
 
-void answer_peak(int gametablet[ROW][COL]) {
-    int cord_i, cord_j, rez_num;
-    int board[ROW][COL];
-    
+int load_file(int array[ROW - 1][COL]) {
+
+    int task_num = 1;
+    char fname[10];
+
+    sprintf(fname, "%d", task_num);
+
     FILE* pf;
     pf = fopen(strcat(fname, ".txt"), "r");
 
@@ -60,12 +66,19 @@ void answer_peak(int gametablet[ROW][COL]) {
         return -1;
     }
 
-    for (int i = 0; i < ROW; i++)
+    for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < COL; j++)
-            fscanf(pf, "%c ", &board[i][j]);
+        for (int j = 0; j < 10; j++)
+            fscanf(pf, "%d ", &array[i][j]);
     }
+    fclose(pf);
+    return 0;
+}
 
+void answer_peak(int gametablet[ROW][COL]) {
+    int cord_i, cord_j, rez_num;
+    int checker_array[ROW - 1][COL];
+    load_file(checker_array);
 
     printf("\n");
     printf("Выберите координату X (0-9): ");
@@ -82,7 +95,7 @@ void answer_peak(int gametablet[ROW][COL]) {
         printf("Выберите число, которое хотите вставить: ");
         scanf("%d", &rez_num);
         getchar();
-        if (rez_num == board[0][cord_j]) {
+        if (rez_num == checker_array[cord_i][cord_j]) {
             gametablet[cord_i][cord_j] = rez_num;
         }
         else {
@@ -94,7 +107,7 @@ void answer_peak(int gametablet[ROW][COL]) {
         printf("Выберите число, которое хотите вставить: ");
         scanf("%d", &rez_num);
         getchar();
-        if (rez_num == board[1][cord_j]) {
+        if (rez_num == checker_array[cord_i][cord_j]) {
             gametablet[cord_i][cord_j] = rez_num;
         }
         else {
@@ -106,7 +119,7 @@ void answer_peak(int gametablet[ROW][COL]) {
         printf("Выберите число, которое хотите вставить: ");
         scanf("%d", &rez_num);
         getchar();
-        if (rez_num == board[2][cord_j]) {
+        if (rez_num == checker_array[cord_i][cord_j]) {
             gametablet[cord_i][cord_j] = rez_num;
         }
         else {
@@ -118,7 +131,7 @@ void answer_peak(int gametablet[ROW][COL]) {
         printf("Выберите число, которое хотите вставить: ");
         scanf("%d", &rez_num);
         getchar();
-        if (rez_num == board[3][cord_j]) {
+        if (rez_num == checker_array[cord_i][cord_j]) {
             gametablet[cord_i][cord_j] = rez_num;
         }
         else {
@@ -130,7 +143,7 @@ void answer_peak(int gametablet[ROW][COL]) {
         printf("Выберите число, которое хотите вставить: ");
         scanf("%d", &rez_num);
         getchar();
-        if (rez_num == board[4][cord_j]) {
+        if (rez_num == checker_array[cord_i][cord_j]) {
             gametablet[cord_i][cord_j] = rez_num;
         }
         else {
@@ -144,61 +157,34 @@ void answer_peak(int gametablet[ROW][COL]) {
     }
 }
 
-void finish_flag() {
+void finish_flag_easy() {
 
     int game_tablet[ROW][COL] = {
-        {5, -1, 0, -1, 7, -1, 1, -1, -1, -1},
+        {5, 3, 0, 8, 7, -1, 1, -1, -1, -1},
         {6, 8, 4, 9, -1, 5, -1, 2, 1, 0},
-        {-1, -1, -1, -1, 6, 1, -1, 5, -1, 9},
-        {-1, -1, -1, 4, -1, -1, 8, -1, 7, -1},
-        {7, -1, 3, 5, -1, -1, 6, -1, 1, 9},
+        {-1,7, 3, -1, 6, 1, 4, 5, -1, 9},
+        {-1, 5, 1, 4, -1, 0, 8, -1, 7,6},
+        {7, 0, 3, 5, -1, -1, 6, -1, 1, 9},
         {20, 23, 11, 28, 27, 12, 26, 27, 23, 28}
     }; /*указание начального игрового поля*/
 
+    int final_checker[ROW - 1][COL];
+    load_file(final_checker);
+
     int game_over = 0;
-   
+
     while (game_over != 1) {
         int flag = 0;
-        int integer = 0;
 
-        while (integer < ROW - 1) {
-            switch (integer) {
-            case 0:
-                for (int j = 0; j < COL; j++) {
-                    if (game_tablet[0][j] == ROW_1[j]) flag += 1;
-                }
-                break;
-
-            case 1:
-                for (int j = 0; j < COL; j++) {
-                    if (game_tablet[1][j] == ROW_2[j]) flag += 1;
-                }
-                break;
-
-            case 2:
-                for (int j = 0; j < COL; j++) {
-                    if (game_tablet[2][j] == ROW_3[j]) flag += 1;
-                }
-                break;
-
-            case 3:
-                for (int j = 0; j < COL; j++) {
-                    if (game_tablet[3][j] == ROW_4[j]) flag += 1;
-                }
-                break;
-
-            case 4:
-                for (int j = 0; j < COL; j++) {
-                    if (game_tablet[4][j] == ROW_5[j]) flag += 1;
-                }
-                break;
+        for (int i = 0; i < ROW - 1; i++) {
+            for (int j = 0; j < COL; j++) {
+                if ((int)game_tablet[i][j] == final_checker[i][j]) flag += 1;
             }
-            integer++;
         }
 
         if (flag == 50) {
             game_over++;
-            printf("\nВЫЙГРЫШ");
+            printf("\n\tВы выйграли! Поздравляем!");
         }
 
         else {
@@ -214,4 +200,154 @@ void finish_flag() {
             его на правильность ввода*/
         }
     }
+}
+
+void finish_flag_normal() {
+
+    int game_tablet[ROW][COL] = {
+        {5, -1, 0, -1, 7, -1, 1, -1, -1, -1},
+        {6, 8, 4, 9, -1, 5, -1, 2, 1, 0},
+        {-1, -1, -1, -1, 6, 1, -1, 5, -1, 9},
+        {-1, -1, -1, 4, -1, -1, 8, -1, 7, -1},
+        {7, -1, 3, 5, -1, -1, 6, -1, 1, 9},
+        {20, 23, 11, 28, 27, 12, 26, 27, 23, 28}
+    }; /*указание начального игрового поля*/
+
+    int final_checker[ROW - 1][COL];
+    load_file(final_checker);
+
+    int game_over = 0;
+
+    while (game_over != 1) {
+        int flag = 0;
+
+        for (int i = 0; i < ROW - 1; i++) {
+            for (int j = 0; j < COL; j++) {
+                if ((int)game_tablet[i][j] == final_checker[i][j]) flag += 1;
+            }
+        }
+
+        if (flag == 50) {
+            game_over++;
+            printf("\n\tВы выйграли! Поздравляем!");
+        }
+
+        else {
+            draw_screen(game_tablet, ROW, COL);
+            /*функция отрисовки игрового поля
+            game_tablet - двумерный массив
+            height - высота поля (кол-во строк)
+            width - ширина поля (кол-во столбцов)*/
+
+            answer_peak(game_tablet);
+            /*функция выбора координаты на игровом поле
+            и числа для постановки его на поле и проверки
+            его на правильность ввода*/
+        }
+    }
+}
+
+void finish_flag_hard() {
+
+    int game_tablet[ROW][COL] = {
+        {-1, -1, 0, -1, 7, -1, 1, -1, -1, -1},
+        {6, -1, 4, 9, -1, 5, -1, 2, 1, 0},
+        {-1, -1, -1, -1, 6, 1, -1, -1, -1, 9},
+        {-1, -1, -1, 4, -1, -1, 8, -1, 7, -1},
+        {7, -1, 3, -1, -1, -1, 6, -1, 1, -1},
+        {20, 23, 11, 28, 27, 12, 26, 27, 23, 28}
+    }; /*указание начального игрового поля*/
+
+    int final_checker[ROW - 1][COL];
+    load_file(final_checker);
+
+    int game_over = 0;
+
+    while (game_over != 1) {
+        int flag = 0;
+
+        for (int i = 0; i < ROW - 1; i++) {
+            for (int j = 0; j < COL; j++) {
+                if ((int)game_tablet[i][j] == final_checker[i][j]) flag += 1;
+            }
+        }
+
+        if (flag == 50) {
+            game_over++;
+            printf("\n\tВы выйграли! Поздравляем!");
+        }
+
+        else {
+            draw_screen(game_tablet, ROW, COL);
+            /*функция отрисовки игрового поля
+            game_tablet - двумерный массив
+            height - высота поля (кол-во строк)
+            width - ширина поля (кол-во столбцов)*/
+
+            answer_peak(game_tablet);
+            /*функция выбора координаты на игровом поле
+            и числа для постановки его на поле и проверки
+            его на правильность ввода*/
+        }
+    }
+}
+
+void choose_path() {
+    int menu_rez;
+    scanf("%d", &menu_rez);
+
+    switch (menu_rez) {
+    case 1:
+
+        printf("\nВыберите уровень сложности: ");
+        printf("\n1 - Лёгкий");
+        printf("\n2 - Средний");
+        printf("\n3 - Сложный");
+        printf("\n");
+
+        int rez_difficulty;
+        scanf("%d", &rez_difficulty);
+
+        switch (rez_difficulty) {
+        case 1:
+            finish_flag_easy();
+            break;
+        case 2:
+            finish_flag_normal();
+            break;
+        case 3:
+            finish_flag_hard();
+            break;
+        default:
+            printf("Ошибка! Неверно введён уровень сложности!");
+            break;
+        }
+        break;
+    case 2:
+        printf("\nЧислобус - головоломка, представляющая собой прямоугольную таблицу из девяти столбцов.\n");
+        printf("В некоторых клетках уже стоят цифры от 1 до 9, остальные пусты.\n");
+        printf("Для них нужно найти подходящие цифры. В нижней строке проставлены суммы для каждого столбца.\n");
+        printf("Цифры в клетках касающиеся друг друга сторонами или углами должны отличаться.\n");
+        printf("Также цифры не могут повторяться в строках. В столбцах повторение цифр разрешено.\n\n");
+
+        menu_peak();
+        break;
+    case 3:
+        printf("\nИгра окончена!");
+        break;
+    default:
+        printf("\nВведено неправильное число!");
+        break;
+    }
+
+}
+void menu_peak() {
+
+    printf("Игра ЧИСЛОБУС\n\n");
+    printf("Выберите:\n");
+    printf("1 - начать игру и выбрать уровень сложности.\n");
+    printf("2 - правила игры.\n");
+    printf("3 - выйти из игры.\n\n");
+
+    choose_path();
 }
